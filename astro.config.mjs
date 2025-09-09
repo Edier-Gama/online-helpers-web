dd9ffa9aa50b80bc4f6ad41c64b89db124c22b4c
Edier Santiago Gama <ediersantiago7@gmail.com>
2025-09-08 22:59:10 -0500

    Header and Main page almost completed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,16 @@ import { defineConfig } from 'astro/config';
 // @ts-check
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react({
+      experimentalReactChildren: true,
+      experimentalDisableStreaming: true,
+  })],
 });
